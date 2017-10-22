af78007f836d451aed32b7cc62e7abdef40c2e6f
Marcus Baw <marcusbaw@gmail.com>
2017-10-22 13:43:32 +0100

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: basic-plugin
+# about: A super simple plugin to demonstrate how plugins work
+# version: 0.0.1
+# authors: pacharanero
+# url: http://www.google.com
+
+enabled_site_setting :sms_notify_enabled
